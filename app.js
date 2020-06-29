c96cc82b8d6766f8c38c80cb5cffe97f3fee50e1
shashank-naik123 <naikshashank12.sn@gmail.com>
2020-06-29 22:07:50 +0530

    new ui

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const https=require("https")
 const bodyParser=require("body-parser")
 const app=express()
 app.use(bodyParser.urlencoded({extended:true}))
+
+app.set('view engine','ejs')
+let temp ="";
+let descr="";
+let icon="";
+let imageUrl="";
 app.get("/",function(req,res){
-    res.sendFile(__dirname+"/index.html")
+  res.render("weather")
     })
 app.use(express.static("public"));
 app.post("/",function(req,res){
@@ -18,21 +24,23 @@ app.post("/",function(req,res){
     response.on("data",function(data){
 
       const weatherData=JSON.parse(data);
-      const temp=weatherData.main.temp
-      const descr=weatherData.weather[0].description
-      const icon=weatherData.weather[0].icon
-      const imageUrl="http://openweathermap.org/img/wn/"+icon+"@2x.png"
-      // const image = __dirname+"images/Weather.png"
-      console.log(descr);
-      // res.write("<img  src="+image+">")
-    
-      res.write("<body>")
-      res.write("<style>  body{ background-color: #ddf3f5;text-align: center;font-family: 'Ubuntu', sans-serif;margin-top: 50px;}</style>")
-      res.write("<h1>Temperature In "+city+" is "+temp+" degree Celsius.</h1>")
-      res.write("<h3>Weather in "+city+" is "+descr+" !</h3>")
-      res.write("<img src="+imageUrl+">")
-      res.write("</body>")
-      res.send()
+      temp=weatherData.main.temp
+      descr=weatherData.weather[0].description
+      icon=weatherData.weather[0].icon
+      imageUrl="http://openweathermap.org/img/wn/"+icon+"@2x.png"
+      res.render("result",{temp:temp,city:city,description:descr,imgUrl:imageUrl})
+
+
+
+
+
+      // res.write("<body>")
+      // res.write("<style>  body{ background-color: #ddf3f5;text-align: center;font-family: 'Ubuntu', sans-serif;margin-top: 50px;}</style>")
+      // res.write("<h1>Temperature In "+city+" is "+temp+" degree Celsius.</h1>")
+      // res.write("<h3>Weather in "+city+" is "+descr+" !</h3>")
+      // res.write("<img src="+imageUrl+">")
+      // res.write("</body>")
+      // res.send()
 })
 })
 })
